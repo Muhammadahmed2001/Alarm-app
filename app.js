59d08fb91b8b10996baa7d845c09a96c232442f5
Ahmed Karim <121400813+Muhammadahmed2001@users.noreply.github.com>
2023-11-22 19:24:13 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -3,9 +3,20 @@ var time = document.getElementById("time")
 date.innerHTML = moment().format('Do MMMM YYYY');
 time.innerHTML = moment().format('h:mm:ss a')
 
-
-
-
 setInterval(() => {
     time.innerHTML = moment().format('h:mm:ss a')
 }, 1000);
+
+
+var inputTime = document.getElementById("input-time")
+var dateInput = document.getElementById("date-input")
+var alarmName = document.getElementById("alarm-name")
+var amPm = ""
+
+var getTime = () =>{
+    
+    // if()
+    
+
+// console.log("======> time " + inputTime.value , "======> date " + dateInput.value , "======> name " + alarmName.value)
+}
