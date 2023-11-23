8f9f40edb714312b9da7ea4e1947455aacc0a692
Ahmed Karim <muhammadahmedkarimnagda@gmail.com>
2023-11-24 01:29:50 +0500

    complete app without pwa

--- app.js
+++ app.js
@@ -1,22 +1,48 @@
-var date = document.getElementById("date")
-var time = document.getElementById("time")
-date.innerHTML = moment().format('Do MMMM YYYY');
-time.innerHTML = moment().format('h:mm:ss a')
+var date = document.getElementById("date");
+var time = document.getElementById("time");
+date.innerHTML = moment().format("Do MMMM YYYY");
+time.innerHTML = moment().format("h:mm:ss a");
 
+var htmlTime = "";
+var amPm = "";
+var notification = false;
 setInterval(() => {
-    time.innerHTML = moment().format('h:mm:ss a')
+  time.innerHTML = moment().format("h:mm:ss a");
+  //   htmlTime = moment(time.innerHTML, "h:mm:ss a").format("hh:mm:ss a");
+  console.log(time.innerHTML.slice(0, 8) === amPm.slice(0, 8));
 }, 1000);
 
+var inputTime = document.getElementById("input-time");
 
-var inputTime = document.getElementById("input-time")
-var dateInput = document.getElementById("date-input")
-var alarmName = document.getElementById("alarm-name")
-var amPm = ""
+var alarmName = document.getElementById("alarm-name");
+var timeList = document.getElementById("time-list");
+var listNo = 0;
 
-var getTime = () =>{
+var getTime = () => {
+  listNo++;
+  amPm = moment(inputTime.value, "hh:mm").format("hh:mm:ss a");
+  if(alarmName.value && inputTime.value  !== ""){
+      timeList.innerHTML += `
+        <tr>
+        <th scope="row">${listNo}</th>
+        <td>${alarmName.value}</td>
+        <td>${amPm.slice(0,5)+ amPm.slice(8)}</td>
+        </tr>
+        `;
+        inputTime.value = "";
+        alarmName.value = "";
+    }else{
+        Swal.fire({
+            title: "The warning?",
+            text: "Enter tha both value",
+            icon: "warning"
+          });
+   
+    }
     
-    // if()
-    
 
-// console.log("======> time " + inputTime.value , "======> date " + dateInput.value , "======> name " + alarmName.value)
-}
+  //   console.log(amPm);
+  // if()
+
+  // console.log("======> time " + inputTime.value , "======> date " + dateInput.value , "======> name " + alarmName.value)
+};
