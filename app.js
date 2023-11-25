0fe0994d7efcff2186c20056363d12140e6b01bd
Ahmed Karim <muhammadahmedkarimnagda@gmail.com>
2023-11-25 14:45:25 +0500

    complete app

--- app.js
+++ app.js
@@ -14,13 +14,7 @@ if ("serviceWorker" in navigator) {
     })
       .catch(err => console.log("service worker not registered", err))
   })
-  navigator.serviceWorker.ready.then((swReg)=>{
-    var option = {
-      message : "This is Message Body"
-    };
-    swReg.showNotification("This is Message title", option)
-  })
-}
+} 
 
 
 
@@ -30,32 +24,63 @@ var date = document.getElementById("date");
 var time = document.getElementById("time");
 date.innerHTML = moment().format("Do MMMM YYYY");
 time.innerHTML = moment().format("h:mm:ss a");
+var inputTime = document.getElementById("input-time");
+var alarmName = document.getElementById("alarm-name");
+var timeList = document.getElementById("time-list");
 
 var htmlTime = "";
 var amPm = "";
 var notification = false;
 setInterval(() => {
-  time.innerHTML = moment().format("h:mm:ss a");
+  time.innerHTML = moment().format("hh:mm:ss a");
   //   htmlTime = moment(time.innerHTML, "h:mm:ss a").format("hh:mm:ss a");
-  // console.log(time.innerHTML.slice(0, 8) === amPm.slice(0, 8));
+  // console.log(time.innerHTML.slice(0, 8) , amPm.slice(0, 8));
+  if(time.innerHTML.slice(0, 8) === amPm.slice(0, 8)){
+    navigator.serviceWorker.ready.then((alert)=>{
+      var option = {
+        body:"Today at " + amPm.slice(0,5) + amPm.slice(8),
+        icon:"/assets/icons/icon-512x512.png"
+        
+
+        
+      };
+      alert.showNotification("AK'Reminder" , option)
+    })
+  }
+
+
+
+
+
+
+  
 }, 1000);
+// navigator.serviceWorker.ready.then((alert)=>{
+//   var option = {
+//     body:"Today at " + amPm.slice(0,5) + amPm.slice(8),
+//     icon:"/assets/icons/icon-512x512.png"
+    
 
-var inputTime = document.getElementById("input-time");
+    
+//   };
+//   alert.showNotification(alarmName.value,option)
+// })
 
-var alarmName = document.getElementById("alarm-name");
-var timeList = document.getElementById("time-list");
+
+
+
 var listNo = 0;
 
 var getTime = () => {
-    amPm = moment(inputTime.value, "hh:mm").format("hh:mm:ss a");
-    if(alarmName.value && inputTime.value  !== ""){
-        listNo++;
-        timeList.innerHTML += `
-        <tr>
-        <th scope="row">${listNo}</th>
-        <td>${alarmName.value}</td>
-        <td>${amPm.slice(0,5)+ amPm.slice(8)}</td>
-        </tr>
+  amPm = moment(inputTime.value, "hh:mm").format("hh:mm:ss a");
+  if(alarmName.value && inputTime.value  !== ""){
+    listNo++;
+    timeList.innerHTML += `
+    <tr>
+    <th scope="row">${listNo}</th>
+    <td>${alarmName.value}</td>
+    <td>${amPm.slice(0,5)+ amPm.slice(8)}</td>
+    </tr>
         `;
         inputTime.value = "";
         alarmName.value = "";
@@ -69,8 +94,8 @@ var getTime = () => {
     }
     
 
-  //   console.log(amPm);
+    //   console.log(amPm);
   // if()
-
+  
   // console.log("======> time " + inputTime.value , "======> date " + dateInput.value , "======> name " + alarmName.value)
 };
